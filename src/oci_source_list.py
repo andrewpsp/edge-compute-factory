7cffbed7280a128ea9f2a4c59eba52369538e9b4
Thompson, Andrew (P3017661) <c-andrew.thompson@charter.com>
2023-03-02 18:23:17 -0500

    set up of the oci source list

---
+++ src/oci_source_list.py
@@ -0,0 +1,33 @@
+container_registries_list = [
+    {
+        "cloud_provider": "Amazon Web Services",
+        "registry": "Amazon Elastic Container Registry (ECR)"
+    },
+    {
+        "cloud_provider": "Microsoft Azure",
+        "registry": "Azure Container Registry (ACR)"
+    },
+    {
+        "cloud_provider": "Google Cloud Platform",
+        "registry": "Google Container Registry (GCR)"
+    },
+    {
+        "cloud_provider": "IBM Cloud",
+        "registry": "IBM Cloud Container Registry"
+    },
+    {
+        "cloud_provider": "Oracle Cloud",
+        "registry": "Oracle Cloud Infrastructure Registry (OCIR)"
+    }
+]
+
+container_registries_cloud = [
+    "https://hub.docker.com", 
+    "https://gcr.io", 
+    "https://quay.io", 
+    "https://registry.gitlab.com", 
+    "https://registry.hub.docker.com",
+    "https://registry.access.redhat.com",
+    "https://docker.pkg.github.com"
+]
+
